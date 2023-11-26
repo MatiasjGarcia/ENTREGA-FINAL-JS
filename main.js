4411f223af6da7a90b15137e43957af1ea322625
matias garcia <matiasjgarcia@outlook.com>
2023-11-26 16:58:30 -0300

    Subida a git

---
+++ main.js
@@ -0,0 +1,56 @@
+// Datos de productos
+const productos = [
+    { id: 1, nombre: "Remera", precio: 10.00, },
+    { id: 2, nombre: "Pantalon", precio: 12.00 },
+    { id: 3, nombre: "Campera", precio: 16.00 }
+    ];
+
+  // Carrito de compras
+    const carrito = [];
+
+  // Renderizar la lista de productos
+    function renderizarProductos() {
+    const productosContainer = document.getElementById("productos");
+    productosContainer.innerHTML = "<h2>Productos</h2>";
+      
+    productos.forEach(producto => {
+        const botonAgregar = document.createElement("button");
+        botonAgregar.innerText = "Agregar al carrito";
+        botonAgregar.addEventListener("click", () => agregarAlCarrito(producto));
+
+    const productoDiv = document.createElement("div");
+    productoDiv.innerHTML = `<p>${producto.nombre} - $${producto.precio.toFixed(2)}</p>`;
+    productoDiv.appendChild(botonAgregar);
+
+    productosContainer.appendChild(productoDiv);
+    });
+    }
+
+  // Agregar un producto al carrito
+    function agregarAlCarrito(producto) {
+    carrito.push(producto);
+    actualizarCarrito();
+    }
+
+  // Actualizar carrito y total
+    function actualizarCarrito() {
+    const carritoContainer = document.getElementById("carrito");
+    const totalElement = document.getElementById("total");
+
+    carritoContainer.innerHTML = "";
+    let total = 0;
+
+    carrito.forEach(producto => {
+        const li = document.createElement("li");
+        li.innerText = `${producto.nombre} - $${producto.precio.toFixed(2)}`;
+        carritoContainer.appendChild(li);
+
+    total += producto.precio;
+    
+    });
+
+    totalElement.innerText = total.toFixed(2);
+    }
+
+  // Iniciar pagina
+    renderizarProductos();
